612b8738df98bf44f216e5edaa7ba543a116fc95
lassel <larpatin@gmail.com>
2020-10-18 17:56:06 +0300

    jee

--- ORIG/main.js
+++ ORIG/main.js
@@ -6,22 +6,38 @@ amqp.connect('amqp://localhost', function(error0, connection) {
   if (error0) {
     throw error0;
   }
+
+
   connection.createChannel(function(error1, channel) {
     if (error1) {
       throw error1;
     }
-    var exchange = 'logs';
+    var exchange = 'topic';
     var msg = process.argv.slice(2).join(' ') || 'Hello World!';
 
     channel.assertExchange(exchange, 'fanout', {
       durable: false
     });
-    channel.publish(exchange, '', Buffer.from(msg));
-    console.log(" [x] Sent %s", msg);
+
+    while(true) {
+      channel.publish(exchange, '', Buffer.from(msg));
+      console.log(" [x] Sent %s", msg);
+      sleep(3000);
+     }
+
   });
+
+
 
   setTimeout(function() { 
     connection.close(); 
     process.exit(0); 
   }, 500);
 });
+
+await sleep(1000)
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+} 

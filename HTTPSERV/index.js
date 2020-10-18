e295a65e75c3a7f0a2e752f894d38211db794aee
lassel <larpatin@gmail.com>
2020-10-18 17:46:28 +0300

    add

---
+++ HTTPSERV/index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var querystring = require('querystring')
+var address = 'http://127.0.0.1'
+var port = 8080;
+
+http.createServer(function (req, res) {
+
+
+    
+}).listen(port);
+console.log(`Server running at ${address}:${port}`);
